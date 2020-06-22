2dcf23f2f91324ac930d88b689db3a51e0fcacb0
stuti_43 <stuti.mohgaonkar@gmail.com>
2020-06-22 17:39:55 +0530

    removed history mode

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,6 @@ module.exports = {
   "transpileDependencies": [
     "vuetify"
   ],
-  baseUrl: '',
   publicPath: process.env.NODE_ENV === 'production'
   ? '/' + process.env.NMCreations + '/'
   : '/'

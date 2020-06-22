bcd7e23ca7a51eb6ce5ef16bb44c9125a336ce87
stuti_43 <stuti.mohgaonkar@gmail.com>
2020-06-22 18:55:41 +0530

    change base

--- src/router/index.js
+++ src/router/index.js
@@ -16,7 +16,7 @@ Vue.use(VueRouter)
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
   },
   {
     path: '/projects',
@@ -38,7 +38,7 @@ Vue.use(VueRouter)
 
 const router = new VueRouter({
   mode: 'history',
-  base: process.env.BASE_URL,
+  base: '/',
   routes,
   scrollBehavior (to,from,savedPosition) {
     if(savedPosition) {

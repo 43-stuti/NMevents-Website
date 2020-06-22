d737d6c10cb38d866c8ce90063ecc5ab0c642e93
stuti_43 <stuti.mohgaonkar@gmail.com>
2020-06-22 17:30:54 +0530

    basUrl updates

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ module.exports = {
   "transpileDependencies": [
     "vuetify"
   ],
+  baseUrl: '',
   publicPath: process.env.NODE_ENV === 'production'
   ? '/' + process.env.NMCreations + '/'
   : '/'
